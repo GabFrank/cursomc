086e79b262b60328f8792e19afbbb4dbe630fdb6
Gabriel Franco <francogabrielpy@outlook.com>
2019-12-18 21:23:29 -0300

    criamos operacao de instanciacao, funcionando

--- src/main/java/com/franco/dev/CursomcApplication.java
+++ src/main/java/com/franco/dev/CursomcApplication.java
@@ -1,13 +1,32 @@
 package com.franco.dev;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.franco.dev.domain.Categoria;
+import com.franco.dev.repository.CategoriaRepository;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner{
+	
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
 	}
 
 }
