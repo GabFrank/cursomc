6df11a5bff00367fc7599e06de7b291a9221d2c0
Gabriel Franco <francogabrielpy@outlook.com>
2019-12-19 22:12:19 -0300

    Lanzando exepciones al no encontrar una categoria, funcionando

--- src/main/java/com/franco/dev/resources/CategoriaResource.java
+++ src/main/java/com/franco/dev/resources/CategoriaResource.java
@@ -1,7 +1,5 @@
 package com.franco.dev.resources;
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.franco.dev.domain.Categoria;
 import com.franco.dev.service.CategoriaService;
 
+import javassist.tools.rmi.ObjectNotFoundException;
+
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
@@ -20,7 +20,7 @@ public class CategoriaResource {
 	private CategoriaService service;
 	
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
-	public ResponseEntity<?> find(@PathVariable Integer id) {
+	public ResponseEntity<?> find(@PathVariable Integer id) throws ObjectNotFoundException {
 		
 		Categoria obj = service.buscar(id);
 		
--- src/main/java/com/franco/dev/service/CategoriaService.java
+++ src/main/java/com/franco/dev/service/CategoriaService.java
@@ -8,15 +8,18 @@ import org.springframework.stereotype.Service;
 import com.franco.dev.domain.Categoria;
 import com.franco.dev.repository.CategoriaRepository;
 
+import javassist.tools.rmi.ObjectNotFoundException;
+
 @Service
 public class CategoriaService {
 	
 	@Autowired
 	private CategoriaRepository repo;
 	
-	public Categoria buscar(Integer id) {
+	public Categoria buscar(Integer id) throws ObjectNotFoundException{
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null); 
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Categoria não encontrada! ID : " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 
 }
